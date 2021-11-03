2869ee80475d38414a97625cf6d1a0da5e3ade86
Deepeshac2890 <deepeshac290@gmail.com>
2021-11-03 13:26:14 +0530

    Code has been refactored.

--- lib/profile/bloc.dart
+++ lib/profile/bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:abs_fitness/Model/ProfileModel.dart';
-import 'package:abs_fitness/Resources/W&FConstants.dart';
 import 'package:abs_fitness/Resources/StringConstants.dart';
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
